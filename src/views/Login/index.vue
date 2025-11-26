16d5119aaa5e334df6211e33292cb7d56fb560ca
ht <htht0823@qq.com>
2025-11-26 16:32:14 +0800

    自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,6 +5,7 @@ import { ref } from "vue";
 const form = ref({
   account: "",
   password: "",
+  agree: true,
 });
 //2.准备规则对象
 const rules = {
@@ -26,6 +27,17 @@ const rules = {
       max: 14,
       message: "密码长度为6-14位",
       trigger: "blur",
+    },
+  ],
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        if (value) {
+          callback();
+        } else {
+          callback(new Error("请勾选协议"));
+        }
+      },
     },
   ],
 };
@@ -65,8 +77,10 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large"> 我已同意隐私条款和服务条款 </el-checkbox>
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
+                  我已同意隐私条款和服务条款
+                </el-checkbox>
               </el-form-item>
               <el-button size="large" class="subBtn">点击登录</el-button>
             </el-form>
