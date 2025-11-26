dc4ed8a0d6ec33396d99757679f511914f4bb8da
ht <htht0823@qq.com>
2025-11-26 18:15:05 +0800

    token请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 //axios基础封装
 import axios from "axios";
 import { ElMessage } from "element-plus";
+import { useUserStore } from "@/stores/user";
 
 const httpInstance = axios.create({
   //基地址
@@ -11,6 +12,13 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
+    //1.从pinia获取token数据
+    const userStore = useUserStore();
+    //2.按照后端的要求进行拼接
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e)
